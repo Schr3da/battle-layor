2e1db41b042253151b0ea30461ef7d851dbadbe3
Georg Strieder <gstrieder@dcodeit.com>
2019-03-28 19:17:30 +0100

    coming close to fix ray casting

--- client/src/canvas/Game.ts
+++ client/src/canvas/Game.ts
@@ -9,77 +9,80 @@ import { updateView } from "./Renderer";
 
 export class Game {
 
-    private animationFrameHandler: any;
-    private updateHandler: any;
-    private renderer: PIXI.Application;
-    private assets: AssetManager;
-    private controls: Controls;
-    private map: number[];
-    private player: Player | null = null;
+	private animationFrameHandler: any;
+	private updateHandler: any;
+	private renderer: PIXI.Application;
+	private assets: AssetManager;
+	private scene: PIXI.Container;
+	private controls: Controls;
+	private map: number[];
+	private player: Player | null = null;
 
-    constructor(wrapper: Element) {
-        this.map = [];    
-        this.animationFrameHandler = null;
+	constructor(wrapper: Element) {
+		this.map = [];
+		this.animationFrameHandler = null;
 
-        this.assets = new AssetManager();
-        this.controls = new Controls();
+		PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+		this.renderer = new PIXI.Application({
+			width: Settings.displayWidth,
+			height: Settings.displayHeight,
+		});
+		this.renderer.ticker.add(this.render);
+		wrapper.appendChild(this.renderer.view);
 
-        PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
-        this.renderer = new PIXI.Application({
-            width: Settings.displayWidth, 
-            height: Settings.displayHeight,
-        });
+		this.assets = new AssetManager();
+		this.controls = new Controls();
 
-        this.renderer.ticker.add(this.render);
-        wrapper.appendChild(this.renderer.view);        
-    }
+		this.scene = new PIXI.Container();
+		this.renderer.stage.addChild(this.scene);
+	}
 
-    private render = () => {
-        if ((this.map || []).length === 0) {
-            return;
-        }
-        this.animationFrameHandler = requestAnimationFrame(this.render);
-        this.renderer.render()
+	private render = () => {
+		if ((this.map || []).length === 0) {
+			return;
+		}
+		this.animationFrameHandler = requestAnimationFrame(this.render);
+		this.renderer.render()
 
-    }
+	}
 
-    private update = () => {
-        if (this.player == null) {
-            return;
-        }
+	private update = () => {
+		if (this.player == null) {
+			return;
+		}
 
-        updateView(this.player, this.map, this.assets);    
-    } 
+		updateView(this.scene, this.player, this.map, this.assets);
+	}
 
-    public start() {
-        cancelAnimationFrame(this.animationFrameHandler);
-        this.animationFrameHandler = requestAnimationFrame( this.render );
+	public start() {
+		cancelAnimationFrame(this.animationFrameHandler);
+		this.animationFrameHandler = requestAnimationFrame(this.render);
 
-        clearInterval(this.updateHandler);
-        this.updateHandler = setInterval(this.update, Settings.refreshTime);    
-    }
+		clearInterval(this.updateHandler);
+		this.updateHandler = setInterval(this.update, Settings.refreshTime);
+	}
 
-    public receivedData(d: IWSResponse<any>) {
-        if (d.action !== WSAction.GAME) {
-            return;
-        }
-        
-        switch(d.resource) {
-            case WSResource.MAP:
-                this.map = d.data;
-                this.player = new Player(2, 2, this.map, this.controls);    
-            default:        
-                console.log(d);
-        }
-    }
+	public receivedData(d: IWSResponse<any>) {
+		if (d.action !== WSAction.GAME) {
+			return;
+		}
 
-    public destory() {
-        cancelAnimationFrame(this.animationFrameHandler);
-        clearInterval(this.updateHandler);
-        
-        if (this.renderer != null) {
-            this.renderer.destroy(true);
-        }
-    }
+		switch (d.resource) {
+			case WSResource.MAP:
+				this.map = d.data;
+				this.player = new Player(2, 2, this.map, this.controls);
+			default:
+				console.log(d);
+		}
+	}
+
+	public destory() {
+		cancelAnimationFrame(this.animationFrameHandler);
+		clearInterval(this.updateHandler);
+
+		if (this.renderer != null) {
+			this.renderer.destroy(true);
+		}
+	}
 
 }
--- client/src/common/Entity.ts
+++ client/src/common/Entity.ts
@@ -1,41 +1,41 @@
 export interface IVector2d {
-    x: number;
-    y: number;
+	x: number;
+	y: number;
 }
 
 export abstract class Entity {
 
-    protected position: IVector2d;
-    protected direction: IVector2d;
-    protected plane: IVector2d;
+	protected position: IVector2d;
+	protected direction: IVector2d;
+	protected plane: IVector2d;
 
-    constructor(x: number, y: number) {
-        this.position = { x, y };
-        this.direction = { x: -1, y: 0 };
-        this.plane = { x: 0, y:1 }; 
-    }
+	constructor(x: number, y: number) {
+		this.position = { x, y };
+		this.direction = { x: -1, y: 0 };
+		this.plane = { x: 0, y: 1 };
+	}
 
-    public getPlane() {
-        return this.direction;
-    }
+	public getPlane() {
+		return this.plane;
+	}
 
-    public getPosition(): IVector2d {
-        return this.position;
-    }
+	public getPosition(): IVector2d {
+		return this.position;
+	}
 
-    public setPosition(x: number, y: number) {
-        this.position = { x, y };
-    }
+	public setPosition(x: number, y: number) {
+		this.position = { x, y };
+	}
 
-    public getDirection(): IVector2d {
-        return this.direction;
-    }
+	public getDirection(): IVector2d {
+		return this.direction;
+	}
 
-    public setDirection(x: number, y: number) {
-        this.direction = { x, y };
-    }
+	public setDirection(x: number, y: number) {
+		this.direction = { x, y };
+	}
 
-    abstract update(delta: number);
+	abstract update(delta: number);
 
 }
 
